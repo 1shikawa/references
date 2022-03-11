a5f2d2f7ebff936f5190b5f20950b788acabcda4
1shikawa <ishikawa.toru@gmail.com>
2022-03-11 14:02:13 +0900

    add mac setup

---
+++ mac_setup/README.md
@@ -0,0 +1,2 @@
+# M1 Macを購入したのでセットアップ＆zsh環境をいい感じにした
+https://qiita.com/Y_uuu/items/db274e2d3eba3cd5c0dd
---
+++ mac_setup/setup.sh
@@ -0,0 +1,61 @@
+# HomeBrewのインストール
+if [ ! -x "`which brew`" ]; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew update
+fi
+
+# mas-cliのインストール
+if [ ! -x "`which mas`" ]; then
+  brew install mas
+fi
+
+mas install 539883307  # LINE
+mas install 803453959  # Slack
+mas install 406056744  # Evernote
+mas install 425955336  # Skitch
+mas install 1176895641 # Spark
+mas install 1278508951 # Trello
+mas install 414855915  # WinArchiver Lite
+
+brew install google-japanese-ime --cask
+brew install google-chrome       --cask
+brew install firefox             --cask
+brew install dropbox             --cask
+brew install skype               --cask
+brew install cheatsheet          --cask
+brew install visual-studio-code  --cask
+brew install docker              --cask
+brew install iterm2              --cask
+brew install macdown             --cask
+brew install authy               --cask
+brew install notable             --cask
+brew install postico             --cask
+brew install arduino             --cask
+brew install sequel-pro          --cask
+brew install brooklyn            --cask
+brew install insomnia            --cask
+brew install discord             --cask
+brew install wireshark           --cask
+brew install toggl-track         --cask
+
+brew tap aws/tap
+brew install git
+brew install wget
+brew install asdf
+brew install nvm
+brew install postgresql
+brew install mysql
+brew install peco
+brew install z
+brew install ghq
+brew install starship
+brew install docker-compose
+brew install yarn
+brew install hub
+brew install hey
+brew install imagemagick
+brew install stoplight-studio
+brew install awscli
+brew install aws-sam-cli
+brew install openssh
+brew install gh 
